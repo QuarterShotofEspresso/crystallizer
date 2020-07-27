6f012045f62978c1149b44eaa3a183a23a127c65
RB <ratnodeepb@gmail.com>
2020-07-26 19:12:50 -0700

    ADC is working but flicker is present

--- code/src/main.c
+++ code/src/main.c
@@ -5,26 +5,41 @@
 
 // custom macros
 #define true 1
+#define TUNE_FACTOR 0x7F
+#define TOTAL_TASKS 2
+#define TICK_GCF 1
+#define TICK_SAMPLE_INTERVAL 10
+#define TICK_DISPLAY_INTERVAL 1
 
 // debugging defines
 #define _ENABLE_AD_
 #define _ENABLE_TICK_
-#define _ENABLE_DELAY_ 1
+#define _ENABLE_DELAY_
+
 
 // global variables
-unsigned short gADCsample;
-unsigned char gHundredsDisplay;
-unsigned char gTensDisplay;
-unsigned char gOnesDisplay;
+unsigned short gADCsample = 0;
+unsigned char gHundredsDisplay = 0x00;
+unsigned char gTensDisplay = 0x00;
+unsigned char gOnesDisplay = 0x00;
+unsigned char gTicksPassed = 0;
 
 unsigned char gConversionComplete = 0;
-enum e_states{START, PROCESS, DISPLAY_ONES, DISPLAY_TENS, DISPLAY_HUNDREDS, ERROR} gState = START;
+enum e_states{START, DISPLAY_ONES, DISPLAY_TENS, DISPLAY_HUNDREDS, ERROR} gState = START;
 
 // additional functions
 unsigned char toHexData( unsigned char sum );
-void stateMachine( void );
-void computeData( void );
-void tick( void );
+void execute      ( void );
+void computeData  ( void );
+void tick_sample  ( void );
+void tick_display ( void );
+void initTasks    ( void );
+
+struct task {
+    void (*tick) (void);
+    unsigned char tickInterval;
+    unsigned char ticksPassed;
+} gTasks[TOTAL_TASKS];
 
 int main( void ) {
 
@@ -35,23 +50,26 @@ int main( void ) {
 
 
     // calibrate internal RC clock
-    OSCCAL = ( OSSCAL & 0x80 ) | 0x08;
+    OSCCAL = ( OSCCAL & 0x80 ) | ( 0x7F & TUNE_FACTOR );
 
     #ifdef _ENABLE_AD_
       // initialize the ADC
-      ADMUX =  (1 << MUX2) | (1 << MUX1) | (1 << MUX0); // select PINA7's ADC
+      PRR0 =    (0 << PRADC); // set PRR0 to 1
+      ADMUX =   (1 << MUX2) | (1 << MUX1) | (1 << MUX0); // select PINA7's ADC
       // Enable ADC, Enable Auto Trigger in Free Running Mode, Enable Interrupt, Set prescaler factor to 128
-      ADCSRA = (1 << ADEN) | (1 << ADATE) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); 
-      DIDR0 =  (1 << ADC7D); // disable digital input on PINA7
-      ADCSRA = (1 << ADSC); // begin sampling
+      ADCSRA |= (1 << ADSC) | (1 << ADEN) | (1 << ADATE);
+      //DIDR0 =   (1 << ADC7D); // disable digital input on PINA7
+      //ADCSRA |= (1 << ADSC); // begin sampling
     #endif
+
+    initTasks();
 
     while( true ) {
         #ifdef _ENABLE_TICK_
-          tick();
+          execute();
         #endif
         #ifdef _ENABLE_DELAY_
-          _delay_ms( _ENABLE_DELAY_ );
+          _delay_ms( TICK_GCF );
         #endif
     }
 
@@ -59,9 +77,37 @@ int main( void ) {
 }
 
 
+void initTasks( void ) {
+    gTasks[0].tick = tick_sample;
+    gTasks[0].tickInterval = TICK_SAMPLE_INTERVAL;
+    gTasks[0].ticksPassed = 0;
+
+    gTasks[1].tick = tick_display;
+    gTasks[1].tickInterval = TICK_DISPLAY_INTERVAL;
+    gTasks[1].ticksPassed = 0;
+
+    return;
+}
+
+void execute( void ) {
+
+    for( unsigned char i = 0; i < TOTAL_TASKS; i++ ) {
+        if( gTasks[i].ticksPassed == gTasks[i].tickInterval ) {
+            gTasks[i].tick();
+            gTasks[i].ticksPassed = 0;
+        } else {
+            gTasks[i].ticksPassed++;
+        }
+    }
+
+    return;
+}
+
+
 void computeData( void ) {
 
     // NOTE:: I realized something neat. So I had to delve a bit deeper: https://www.desmos.com/calculator/nephumyfrw
+    gADCsample = ADC;
     unsigned short milliamps = (( gADCsample >> 1 ) * 125 ) >> 7;
     unsigned char hundreds = milliamps / 100;
     unsigned char tens =  (( milliamps % 100 ) / 10 );
@@ -75,14 +121,17 @@ void computeData( void ) {
 }
 
 
-void tick( void ) {
+void tick_sample( void ) {
+    // there's only one state in sample
+    computeData();
+
+}
+
+
+void tick_display( void ) {
    
     // transitions
     switch( gState ) {
-
-        case PROCESS:
-            gState = DISPLAY_HUNDREDS;
-            break;
 
         case DISPLAY_HUNDREDS:
             gState = DISPLAY_TENS;
@@ -93,16 +142,11 @@ void tick( void ) {
             break;
 
         case DISPLAY_ONES:
-            if( gConversionComplete ) {
-                gConversionComplete = 0;
-                gState = PROCESS;
-            } else {
-                gState = DISPLAY_HUNDREDS;
-            }
+            gState = DISPLAY_HUNDREDS;
             break;
 
         case START:
-            gState = PROCESS;
+            gState = DISPLAY_HUNDREDS;
             break;
 
         default:
@@ -114,10 +158,6 @@ void tick( void ) {
 
     // actions
     switch( gState ) {
-
-        case PROCESS:
-            computeData();
-            break;
 
         case DISPLAY_HUNDREDS:
             PORTD = 0x00;
@@ -154,7 +194,6 @@ void tick( void ) {
 unsigned char toHexData( unsigned char sum ) {
 
     switch( sum ) {
-
         case 0: sum = ~0x3F; break;
     	case 1: sum = ~0x06; break;
     	case 2: sum = ~0x5B; break;
@@ -166,14 +205,13 @@ unsigned char toHexData( unsigned char sum ) {
     	case 8: sum = ~0x7F; break;
     	case 9: sum = ~0x67; break;
     	default: sum = ~0x80; break;
-
     }
 
     return sum;
 }
 
-
+/*
 ISR( ADC_vect ) { 
     gADCsample = ADC;
     gConversionComplete = 1;
-}
+}*/
