9dd83e30383f20dc76137485d59862eab5461108
RB <ratnodeepb@gmail.com>
2020-10-08 21:28:57 -0700

    Fixed strobe effect

--- code/src/main.c
+++ code/src/main.c
@@ -1,21 +1,26 @@
-/*	Author: Ratnodeep Bandyopadhyay
+/*
+ * Author: Ratnodeep Bandyopadhyay
+ * 
+ * All rights reserved.
+ * 
  */
-#include <avr/io.h>
-#include <util/delay.h>
 
-// custom macros
-#define true 1
-#define TUNE_FACTOR 0xFF
-#define TOTAL_TASKS 2
-#define TICK_GCF 1
-#define TICK_SAMPLE_INTERVAL 10
-#define TICK_DISPLAY_INTERVAL 1
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+// custom defines
+#define true         1
+#define false        0
+#define RC_CLOCK_CAL 0x7F
+#define TOTAL_TASKS  2
+#define TICK_GCF     10
+#define TICK_SAMPLE_INTERVAL  500
+#define TICK_DISPLAY_INTERVAL 10
 
 // debugging defines
 #define _ENABLE_AD_
 #define _ENABLE_TICK_
 #define _ENABLE_DELAY_
-
 
 // global variables
 unsigned short gADCsample = 0;
@@ -23,6 +28,7 @@ unsigned char gHundredsDisplay = 0x00;
 unsigned char gTensDisplay = 0x00;
 unsigned char gOnesDisplay = 0x00;
 unsigned char gTicksPassed = 0;
+unsigned short gTimeElapsed = 0;
 
 unsigned char gConversionComplete = 0;
 enum e_states{START, DISPLAY_ONES, DISPLAY_TENS, DISPLAY_HUNDREDS, ERROR} gState = START;
@@ -30,11 +36,9 @@ enum e_states{START, DISPLAY_ONES, DISPLAY_TENS, DISPLAY_HUNDREDS, ERROR} gState
 // additional functions
 unsigned char toHexData( unsigned char sum );
 void execute      ( void );
-//void computeData  ( void );
 void tick_sample  ( void );
 void tick_display ( void );
 void initTasks    ( void );
-
 
 struct task {
     void (*tick) (void);
@@ -45,43 +49,60 @@ struct task {
 
 int main( void ) {
 
+    // port initialization and data direction
     DDRA = 0x00; PORTA = 0x00;
     DDRB = 0x00; PORTB = 0x00;
     DDRC = 0xFF; PORTC = 0x00;
     DDRD = 0x07; PORTD = 0x00;
 
     // calibrate internal RC clock
-    OSCCAL = TUNE_FACTOR;
+    OSCCAL = RC_CLOCK_CAL;
 
-    // setting the clock prescaler
-    //CLKPR = 0x80;
-    //CLKPR = 0x80;
+  #ifdef _ENABLE_AD_
+    // initialize the ADC
+    PRR0  =   (0 << PRADC); // disable power reduction for adc
+    ADMUX =   (1 << MUX2) | (1 << MUX1) | (1 << MUX0); // select PINA7's ADC
+    DIDR0 =   (1 << ADC7D); // disable digital input on PINA7
+    // Enable ADC, Enable Auto Trigger in Free Running Mode, Enable Interrupt, Set prescaler factor to 128
+    ADCSRA |= (1 << ADSC) | (1 << ADEN);
+  #endif
 
 
-    #ifdef _ENABLE_AD_
-      // initialize the ADC
-      PRR0 =    (0 << PRADC); // set PRR0 to 0
-      ADMUX =   (1 << MUX2) | (1 << MUX1) | (1 << MUX0); // select PINA7's ADC
-      DIDR0 =   (1 << ADC7D); // disable digital input on PINA7
-      // Enable ADC, Enable Auto Trigger in Free Running Mode, Enable Interrupt, Set prescaler factor to 128
-      ADCSRA |= (1 << ADSC) | (1 << ADEN); //| (1 << ADATE);
-    #endif
+  #ifdef _ENABLE_DELAY_
+    // setup timer0 
+    // ensure countera0 is not doing anything
+    TCCR0A = 0x00;
+    // and timer0b with 1024 prescaler
+    TCCR0B = (1 << CS00); //| (1 << CS00);
+    // enable timrsk0 timer overflow interrupt
+    TIMSK0 = (1 << TOIE0);
+  #endif
+
+    // set the global interrupt enable
+    SREG |= (1 << 7);
 
     initTasks();
 
     while( true ) {
-        #ifdef _ENABLE_TICK_
-          execute();
-        #endif
-        #ifdef _ENABLE_DELAY_
-          _delay_ms( TICK_GCF );
-        #endif
+      #ifdef _ENABLE_TICK_
+        executeTasks();
+      #endif
+      #ifdef _ENABLE_DELAY_
+        while(gTimeElapsed < TICK_GCF);
+        gTimeElapsed = 0;
+      #endif
     }
 
     return 1;
 }
 
 
+
+/*
+ *  initTasks:
+ * initialize Tasks stored in gTasks
+ *
+ */
 void initTasks( void ) {
     gTasks[0].tick = tick_sample;
     gTasks[0].tickInterval = TICK_SAMPLE_INTERVAL;
@@ -94,7 +115,13 @@ void initTasks( void ) {
     return;
 }
 
-void execute( void ) {
+
+/*
+ *  executeTasks
+ * execute all tasks in gTasks
+ *
+ */
+void executeTasks( void ) {
 
     for( unsigned char i = 0; i < TOTAL_TASKS; i++ ) {
         if( gTasks[i].ticksPassed == gTasks[i].tickInterval ) {
@@ -109,6 +136,12 @@ void execute( void ) {
 }
 
 
+
+/*
+ *  tick_sample
+ * FSM controlling the sampling and evaluation of the ADC.
+ *
+ */
 void tick_sample( void ) {
 
     // NOTE:: I realized something neat. So I had to delve a bit deeper: https://www.desmos.com/calculator/nephumyfrw
@@ -129,6 +162,10 @@ void tick_sample( void ) {
 }
 
 
+/*  tick_display:
+ * FSM controlling the seven-segment display
+ *
+ */
 void tick_display( void ) {
    
     // transitions
@@ -192,6 +229,11 @@ void tick_display( void ) {
 }
 
 
+/*  toHexData
+ * Convert a numerical value *sum* to
+ * to the 8-bit representation of  the *sum* on a seven-seg
+ *
+ */
 unsigned char toHexData( unsigned char sum ) {
 
     switch( sum ) {
@@ -211,8 +253,10 @@ unsigned char toHexData( unsigned char sum ) {
     return sum;
 }
 
-/*
-ISR( ADC_vect ) { 
-    gADCsample = ADC;
-    gConversionComplete = 1;
-}*/
+
+// Interrupt Subroutine
+//Wait till the timer interrupt is reached to delay for a particular duration
+ISR( TIMER0_OVF_vect ) {
+    gTimeElapsed++;
+}
+
